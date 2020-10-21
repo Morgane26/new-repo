0481a7b688f2513c5435948a7f120f5668f1bcd7
Morgane Perenzoni <mperenzoni@gmail.com>
2020-10-21 23:06:33 +0200

    fist-commit-with-navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Chefs partenaires',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Nous contacter',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Me connecter',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
